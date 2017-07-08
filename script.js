6605f2c915e24b33ff98ce73f87ea13971edf3ed
Lana Chiad <lanachiad7@gmail.com>
2017-07-08 13:52:11 -0400

    Use const and let instead of var

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(function() {
   // saving dom objects to variables
-  var container = $('container');
-  var bird = $('#bird');
-  var pole = $('.pole');
-  var pole_1 = $('#pole_1');
-  var pole_2 = $('#pole_2');
-  var score = $('#score');
-  var speed_span = $('#speed');
-  var restart_btn = $('#restart_btn');
+  const container = $('container');
+  const bird = $('#bird');
+  const pole = $('.pole');
+  const pole_1 = $('#pole_1');
+  const pole_2 = $('#pole_2');
+  const score = $('#score');
+  const speed_span = $('#speed');
+  const restart_btn = $('#restart_btn');
 
   // saving initial setup
-  var container_width = Number(container.width());
-  var pole_initial_position = Number(pole.css('right'));
-  var pole_initial_height = Number(pole.css('height'));
-  var bird_left = Number(bird.css('left'));
-  var bird_height = Number(bird.height());
-  var speed = 10;
+  let container_width = Number(container.width());
+  let pole_initial_position = Number(pole.css('right'));
+  let pole_initial_height = Number(pole.css('height'));
+  let bird_left = Number(bird.css('left'));
+  let bird_height = Number(bird.height());
+  let speed = 10;
 });
