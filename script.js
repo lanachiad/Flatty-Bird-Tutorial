34ca176dd175dac1b433bbe0aab7232441853dd0
Lana Chiad <lanachiad7@gmail.com>
2017-07-08 14:06:57 -0400

    Fix errors in code -- following tutorial, then will refactor to ES6

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 $(function() {
   // saving dom objects to variables
-  const container = $('#container');
-  const bird = $('#bird');
-  const pole = $('.pole');
-  const pole_1 = $('#pole_1');
-  const pole_2 = $('#pole_2');
-  const score = $('#score');
-  const speed_span = $('#speed');
-  const restart_btn = $('#restart_btn');
+  var container = $('#container');
+  var bird = $('#bird');
+  var pole = $('.pole');
+  var pole_1 = $('#pole_1');
+  var pole_2 = $('#pole_2');
+  var score = $('#score');
+  var speed_span = $('#speed');
+  var restart_btn = $('#restart_btn');
 
   // saving initial setup
-  let container_width = Number(container.width());
-  let pole_initial_position = Number(pole.css('right'));
-  let pole_initial_height = Number(pole.css('height'));
-  let bird_left = Number(bird.css('left'));
-  let bird_height = Number(bird.height());
-  let speed = 10;
+  var container_width = parseInt(container.width());
+  var pole_initial_position = parseInt(pole.css('right'));
+  var pole_initial_height = parseInt(pole.css('height'));
+  var bird_left = parseInt(bird.css('left'));
+  var bird_height = parseInt(bird.height());
+  var speed = 10;
 
-  let the_game = setInterval(function() {
-    var pole_current_position = Number(pole.css('right'));
+  var the_game = setInterval(function() {
+    var pole_current_position = parseInt(pole.css('right'));
 
     // check whether the poles are within the container
     if (pole_current_position > container_width) {
